4a14cf309864b4067a00644dc8381210bc61d95d
SnGmng <38666407+SnGmng@users.noreply.github.com>
2018-04-23 20:28:38 +0200

    Add files via upload

---
+++ src/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UltraCtrlAltF4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ContextMenu cm;
+            MenuItem miCurr;
+            int iIndex = 0;
+
+
+            
+            cm = new ContextMenu();
+
+            
+            miCurr = new MenuItem();
+            miCurr.Index = iIndex++;
+            miCurr.Text = "&Exit";
+            miCurr.Click += new System.EventHandler(Action1Click);
+            cm.MenuItems.Add(miCurr);
+
+            notifyIcon1.Icon = Properties.Resources.ultraaltf4;
+            notifyIcon1.Text = "UltraCtrlAltF4 by SnGmng";
+            notifyIcon1.Visible = true;
+            notifyIcon1.ContextMenu = cm;
+
+            Application.Run();
+        }
+
+        private void Action1Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace UltraCtrlAltF4
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern uint GetWindowThreadProcessId(int hWnd, out int ProcessId);
+
+        [DllImport("user32.dll")]
+        private static extern int GetForegroundWindow();
+
+
+        static KeyboardHook hook = new KeyboardHook();
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // register the event that is fired after the key press.
+            hook.KeyPressed +=
+                new EventHandler<KeyPressedEventArgs>(keyDown);
+            // register the control + alt + F4 combination as hot key.
+            hook.RegisterHotKey(ModifierKeys.Control | ModifierKeys.Alt,
+                Keys.F4);
+
+            Application.Run(new Form1());
+
+            hook.Dispose();
+        }
+
+        public static Process GetActiveProcess()
+        {
+            Int32 hwnd = 0;
+            Int32 pid = 1;
+
+            hwnd = GetForegroundWindow();
+
+            GetWindowThreadProcessId(hwnd, out pid);
+
+            return Process.GetProcessById(pid);
+
+            //string appProcessName = Process.GetProcessById(GetWindowProcessID(hwnd)).ProcessName;
+            //string appExePath = Process.GetProcessById(GetWindowProcessID(hwnd)).MainModule.FileName;
+            //string appExeName = appExePath.Substring(appExePath.LastIndexOf(@"\") + 1);
+            
+        }
+
+        public static void KillActiveProcess()
+        {
+
+            Process p = GetActiveProcess();
+            p.Kill();
+
+        }
+
+        public static void keyDown(object sender, KeyPressedEventArgs e)
+        {
+            Debug.WriteLine("Ctrl-Alt-F4 PRESSED");
+            KillActiveProcess();
+        }
+
+    }
+}
